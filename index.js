a816b794345d0adae33ee66047097a5d36b33c9a
Jeff Griffiths <jeff@eko-recordings.ca>
2026-02-07 13:24:21 -0800

    fixed vibed imports

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
-export * from './ableton-info-macos.js'
-// export * from './ableton-info-windows.js'
-export * from './ableton-prefs.js'
+export * from './lib/ableton-info.js'
+export * from './lib/ableton-prefs.js'
