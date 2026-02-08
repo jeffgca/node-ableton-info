1f535f4534d9e78a993acf16e9ce27d1960afff5
Jeff Griffiths <jeff@eko-recordings.ca>
2026-02-08 11:57:03 -0800

    stubbing out windows support

---
+++ tests/ableton-info-win64.test.js
@@ -0,0 +1,20 @@
+import { expect, test, vi, describe } from 'vitest'
+import { execSync } from 'child_process'
+import { platform } from 'os'
+
+vi.mock('child_process', async () => {
+	const actual = await vi.importActual('child_process')
+	return {
+		...actual,
+		execSync: vi.fn(actual.execSync),
+	}
+})
+
+import AbletonInfoWin64 from '../lib/ableton-info-win64.js'
+
+describe.skipIf(platform() !== 'win32')('AbletonInfoMacOS', () => {
+	test('AbletonInfoMacOS constructor sets platform to darwin', () => {
+		const instance = new AbletonInfoMacOS()
+		expect(instance.platform).toBe('darwin')
+	})
+})
